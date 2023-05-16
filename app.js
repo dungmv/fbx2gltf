af3fc2881030ca524a7be9e834172357565aa588
dungmv <maivandung91@gmail.com>
2023-05-16 10:27:07 +0700

    healthz send ok

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/convert', authenticateJWT, convertRouter);
-app.get('/healthz', (req, res) => { res.send(); })
+app.get('/healthz', (req, res) => { res.send('ok'); })
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
